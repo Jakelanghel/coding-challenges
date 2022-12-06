b89b7631c2f32f306133a3231d1059905ac05a72
Jake <jacobtlanghel@gmail.com>
2022-12-05 20:48:36 -0500

    refactor

--- hex-to-rgb/index.js
+++ hex-to-rgb/index.js
@@ -5,8 +5,11 @@ function hexStringToRGB(hexString) {
   const chart = { a: 10, b: 11, c: 12, d: 13, e: 14, f: 15 };
 
   const useChart = (x) => {
-    x = x.toLowerCase();
-    return chart[x];
+    if (isNaN(x)) {
+      return parseInt(chart[x.toLowerCase()]);
+    } else {
+      return parseInt(x);
+    }
   };
 
   let values = [
@@ -15,37 +18,11 @@ function hexStringToRGB(hexString) {
     hexString.slice(5),
   ];
 
-  values = values.map((value) => {
-    let x = null;
-    let y = null;
-    for (let i = 0; i < value.length; i++) {
-      if (i === 0) {
-        if (isNaN(value[i])) {
-          x = useChart(value[i]) * 16;
-        } else {
-          x = value[i] * 16;
-        }
-      } else {
-        if (isNaN(value[i])) {
-          y = useChart(value[i]);
-        } else {
-          y = value[i];
-        }
-      }
-    }
-    x = parseInt(x);
-    y = parseInt(y);
-    return x + y;
+  values = values.map((value, i) => {
+    return useChart(value[0]) * 16 + useChart(value[1]);
   });
+
   return { r: values[0], g: values[1], b: values[2] };
 }
 
 console.log(hexStringToRGB(tst));
-
-// if (isNaN(v)) {
-//     v = v.toLowerCase();
-//     console.log(chart[v]);
-//     return chart.v;
-//   } else {
-//     return v;
-//   }
