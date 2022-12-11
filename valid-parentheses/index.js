7efdc34630c2b5a8e804f961839f1d82f0c4bba2
Jake <jacobtlanghel@gmail.com>
2022-12-11 00:06:57 -0500

    refactor 1

--- valid-parentheses/index.js
+++ valid-parentheses/index.js
@@ -2,35 +2,31 @@ const tst1 = "(())((()())())"; // true
 const tst2 = "())("; // false
 const tst3 = ")";
 const tst4 = "()()"; // true
-const tst5 = "(())"; // false
+const tst5 = "(())"; // true
 
 function validParentheses(str) {
+  // Create array from the input string
   let inputArr = Array.from(str);
-  const closeIndexArr = [];
+  // Create empty array to hold brackets that have matches
   const openIndexArr = [];
+  // Create empty array to hold index's of closing brackets so you don't count them twice
+  const closeIndexArr = [];
 
+  // Loop over inputArr
   inputArr.forEach((char, i) => {
+    // Check for opening brackets
     if (char === "(") {
-      let hasMatch = false;
+      // Loop over the rest of inputArr starting at opening bracket index
       for (let j = i; j < inputArr.length; j++) {
-        if (inputArr[j] === ")" && !hasMatch) {
-          if (!closeIndexArr.includes(j)) {
-            hasMatch = true;
-
-            closeIndexArr.push(j);
-            openIndexArr.push(i);
-          }
+        if (inputArr[j] === ")" && !closeIndexArr.includes(j)) {
+          closeIndexArr.push(j);
+          openIndexArr.push(i);
+          break;
         }
       }
     }
   });
-
   return inputArr.length === closeIndexArr.length + openIndexArr.length;
 }
 
-console.log(validParentheses(tst4));
-
-// validParentheses(tst3);
-// console.log(closeIndexArr);
-// console.log(openIndexArr);
-// console.log(inputArr);
+console.log(validParentheses(tst2));
