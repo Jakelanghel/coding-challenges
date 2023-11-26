5ea04d8393566d983f31e93eeaccbf8e1b8a84ed
jakelanghel <jakelangheldev@gmail.com>
2023-11-26 14:18:32 -0500

    finish challenge 9

--- spaghetti-code/index.js
+++ spaghetti-code/index.js
@@ -1,64 +1,40 @@
-const test_1 = [
-  "SSSSSASS____",
-  "____________",
-  "SSSSSSBSSSS_",
-  "____________",
-  "SSSSSC______",
+// Ex1
+
+// AAAAAAAA____
+// ____________
+// BBBBBBBBBBB_
+// ____________
+// CCCCCC______
+
+// Answer: B
+
+// Ex2
+
+// AAAAAAAAA
+// ________A__CCC_
+//  B   C  A    C
+// _B___C__A____C_
+//  B   C       C
+// _B___CCCCCCCCC_
+
+// Answer: C
+
+const tst1 = [
+  ["A", "A", "A", "A", "A", "A", "A", "A", "_", "_", "_", "_"],
+  ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "_"],
+  ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
+  ["C", "C", "C", "C", "C", "C", "_", "_", "_", "_", "_", "_"],
 ];
-const test_2 = [
-  "SSSSSSSSS______",
-  "________S__SSS_",
-  " S   S  A    S ",
-  "_S___S__S____S_",
-  " B   S       S ",
-  "_S___SSSSSCSSS_",
-];
 
-const createSpaghettiArr = (plate) => {
-  const noodles = [];
-  let noodle = "";
-
-  plate.forEach((piece) => {
-    for (let i = 0; i < piece.length; i++) {
-      const isAlpha = /^[a-zA-Z]+$/.test(piece[i]);
-      if (isAlpha) {
-        noodle += piece[i];
-      } else {
-        if (noodle !== "") {
-          noodles.push(noodle);
-          noodle = "";
-        }
+const spaghettiCode = (tst1) => {
+  const results = {};
+  tst1.forEach((row) => {
+    row.forEach((char) => {
+      if (/^[A-Z]$/.test(char)) {
       }
-    }
-  });
-  return noodles;
-};
-
-const getLengthsArr = (spaghettiArr) => {
-  return spaghettiArr.map((noodle) => {
-    const data = { id: "S", length: noodle.length };
-    for (let i = 0; i < noodle.length; i++) {
-      if (noodle[i] !== "S") {
-        data.id = noodle[i];
-      }
-    }
-    return data;
+    });
   });
 };
 
-const findLargestPiece = (lengthsArr) => {
-  let largest = lengthsArr[0];
-  lengthsArr.forEach((noodle) => {
-    largest = noodle.length >= largest.length ? noodle : largest;
-  });
-
-  return largest.id;
-};
-
-var spaghettiCode = function (plate) {
-  const spaghettiArr = createSpaghettiArr(plate);
-  const lengthsArr = getLengthsArr(spaghettiArr);
-  return findLargestPiece(lengthsArr);
-};
-
-console.log(spaghettiCode(test_1));
+console.log(spaghettiCode(test1));
